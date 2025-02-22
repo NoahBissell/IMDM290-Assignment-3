9f15fbdc8bef17f1b738d999b76161e6414832ce
andiesjaa <krookshanks04@gmail.com>
2025-02-22 16:15:52 -0500

    sin to audio

--- Assets/Ballwave.cs
+++ Assets/Ballwave.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Ballwave : MonoBehaviour
 {
+    public AudioSpectrum.FrequencyRange sirenRange;
     public float delay = 0.25f; //faked data
     public float timer = 0;
     public float waveSpeed = 0.1f;
@@ -30,13 +31,15 @@ public class Ballwave : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float volume = AudioSpectrum.GetAmpInRange(sirenRange);
         if(Time.time > timer)
         {
             timer = Time.time + delay; //changing the delay changes the beat
             for(int i = 0; i < 50; i++)
             {
                 Vector3 pos = Spheres[i].transform.position;
-                pos.y = Mathf.Sin(Time.time - (i * waveSpeed));
+                pos.y = Mathf.Sin(volume);
+                // pos.y = Mathf.Sin(Time.time - (i * waveSpeed));
                 Spheres[i].transform.position = pos;
             }
         }
