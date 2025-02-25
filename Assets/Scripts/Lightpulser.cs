a30dcbd8b47d1cde406b89d5a1bebc1daf203c5a
andiesjaa <krookshanks04@gmail.com>
2025-02-24 19:01:59 -0500

    changed audio variable

--- Assets/Scripts/Lightpulser.cs
+++ Assets/Scripts/Lightpulser.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class Lightpulser : MonoBehaviour
 {
     public Material material;
-    public AudioSpectrum.FrequencyRange dodoRange;
+    public SpectrumTracker spectrum;
+    public SpectrumTracker.FrequencyRange dodoRange;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +14,7 @@ public class Lightpulser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float volume = AudioSpectrum.GetAmpInRange(dodoRange);
+        float volume = spectrum.GetAmpInRange(dodoRange);
         material.color = Color.white * Mathf.Sin(100 * volume);
     }
 }
